7fbed81a5508722ff4f9d1153357c63b853e501a
Kalvin Krajca <kalman98@kalman98.net>
2017-03-11 22:14:30 -0600

    Made gauges look right, and made them fillable with math.

---
+++ Assets/scripts/BarSize.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarSize : MonoBehaviour {
+
+	public GameObject gaugeInside;
+
+	public int maxSize;
+	public int amount;
+	private float currentSize;
+	private float fullLength;
+
+	void Start ()
+	{
+		fullLength = (float)245.6;
+		CalculateLength (maxSize, amount);
+	}
+
+	void Update ()
+	{
+		CalculateLength (maxSize, amount);
+	}
+
+	void CalculateLength (int max, int amount)
+	{
+		// calculate amount and move bar
+		float width = (((float)amount / (float)max) * fullLength);
+		print (width);
+		// for future reference, these numbers get
+		// multiplied the default width and height
+		// of the GameObject (1, 53)
+		transform.localScale = new Vector2 (width, 1);
+	}
+}
