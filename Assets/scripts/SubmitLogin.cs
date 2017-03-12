cd8def112eeb0ad2360bc9ac4007152bd13d8d8f
Kalvin Krajca <kalman98@kalman98.net>
2017-03-12 18:01:59 -0500

    Added Login/Registry scene with mostly complete code.

---
+++ Assets/scripts/SubmitLogin.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmitLogin : MonoBehaviour {
+
+	public UnityEngine.UI.Button button;
+
+	public UnityEngine.UI.InputField usernameField;
+	public UnityEngine.UI.InputField passwordField;
+	public UnityEngine.UI.InputField emailField;
+
+	private string username;
+	private string password;
+	private string email;
+	// Use this for initialization
+	void Start () {
+		username = null;
+		password = null;
+		button.onClick.AddListener (() => {
+			ProcessForm ();
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ProcessForm ()
+	{
+		username = usernameField.text;
+		password = passwordField.text;
+		email = emailField.text;
+		SendData ();
+	}
+
+	WWW SendData ()
+	{
+		WWWForm data = new WWWForm();
+		data.AddField("cmd", "{\"id\":1,\"username\":\"" + username + "\",\"password\":\"" + password + "\",\"email\":\"" + email + "\"}");
+		string url = "";
+		WWW www = new WWW(url, data);
+		yield return www;
+	}
+}
