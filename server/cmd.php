f8dd18e7408c0c302d921cdcece85df41b59a25f
ParkerMc <nick@reisingerfamily.net>
2017-03-11 18:56:44 -0600

    Changed server to accept POST data not GET

--- server/cmd.php
+++ server/cmd.php
@@ -2,8 +2,8 @@
 include 'settings.php';
 include 'users.php';
 
-if(isset($_GET["cmd"])){
-  $json = json_decode ($_GET["cmd"], true); //decode json from get varable cmd
+if(isset($_POST["cmd"])){
+  $json = json_decode ($_POST["cmd"], true); //decode json from get varable cmd
   if($json["id"] == 1){
     createUser($json);
   }
