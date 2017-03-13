28d35c0e956507a352144aea677a0c61817df54f
ParkerMc <nick@reisingerfamily.net>
2017-03-13 00:25:46 -0500

    Fixed register page

--- Assets/scripts/SubmitLogin.cs
+++ Assets/scripts/SubmitLogin.cs
@@ -10,13 +10,13 @@ public class SubmitLogin : MonoBehaviour {
 	public UnityEngine.UI.InputField passwordField;
 	public UnityEngine.UI.InputField emailField;
 
-	private string username;
+	public string username;
+	public string token;
 	private string password;
 	private string email;
+
 	// Use this for initialization
 	void Start () {
-		username = null;
-		password = null;
 		button.onClick.AddListener (() => {
 			ProcessForm ();
 		});
@@ -32,17 +32,39 @@ public class SubmitLogin : MonoBehaviour {
 		username = usernameField.text;
 		password = passwordField.text;
 		email = emailField.text;
-		SendData ();
+		SendData();
 	}
 
-	IEnumerator SendData ()
+	void ProcessResponce(string responce){
+		JSONObject json = new JSONObject(responce);
+		bool success = json.list[json.keys.IndexOf("success")].b;
+		if (success) {
+			token = json.list [json.keys.IndexOf ("token")].str;
+			username = json.list [json.keys.IndexOf ("username")].str;
+		} else {
+			print ("Someting whent wrong:" + json.list[json.keys.IndexOf("msg")].str);
+		}
+	}
+
+
+	void SendData ()
 	{
 		WWWForm data = new WWWForm();
 		data.AddField("cmd", "{\"id\":1,\"username\":\"" + username + "\",\"password\":\"" + password + "\",\"email\":\"" + email + "\"}");
 		string url = "https://parkermc.ddns.net/RA1/server/cmd.php";
 		WWW www = new WWW(url, data);
+		StartCoroutine(WaitForRequest(www));
+	}
+	private IEnumerator WaitForRequest(WWW www){
 		yield return www;
-		string json = www.text;
 
+		// check for errors
+		if (www.error == null)
+		{
+			Debug.Log("WWW Ok!: " + www.text);
+			ProcessResponce (www.text);
+		} else {
+			Debug.Log("WWW Error: "+ www.error);
+		}    
 	}
 }
